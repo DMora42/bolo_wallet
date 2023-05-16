53741f04ac6669576884e69a1052dd205cd5c1e3
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-16 09:08:16 +0000

    Setting up

--- lib/widgets/navigator/bottom_bar_items.dart
+++ lib/widgets/navigator/bottom_bar_items.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 
+const Color inActiveItem = Colors.blueGrey;
+const Color activeItem = Colors.amber;
+
 List<BottomBarItem> items = [
   const BottomBarItem(
-    inActiveItem: FaIcon(FontAwesomeIcons.wallet, color: Colors.blueGrey),
-    activeItem: FaIcon(FontAwesomeIcons.wallet, color: Colors.blueAccent),
+    inActiveItem: FaIcon(FontAwesomeIcons.wallet, color: inActiveItem),
+    activeItem: FaIcon(FontAwesomeIcons.wallet, color: activeItem),
   ),
   const BottomBarItem(
-    inActiveItem: FaIcon(FontAwesomeIcons.chartSimple, color: Colors.blueGrey),
+    inActiveItem: FaIcon(FontAwesomeIcons.chartSimple, color: inActiveItem),
     activeItem: FaIcon(
       FontAwesomeIcons.chartSimple,
-      color: Colors.blueAccent,
+      color: activeItem,
     ),
   ),
   const BottomBarItem(
     inActiveItem: FaIcon(
       FontAwesomeIcons.rightLeft,
-      color: Colors.blueGrey,
+      color: inActiveItem,
     ),
     activeItem: FaIcon(
       FontAwesomeIcons.rightLeft,
@@ -27,21 +30,21 @@ List<BottomBarItem> items = [
   const BottomBarItem(
     inActiveItem: FaIcon(
       FontAwesomeIcons.users,
-      color: Colors.blueGrey,
+      color: inActiveItem,
     ),
     activeItem: FaIcon(
       FontAwesomeIcons.users,
-      color: Colors.pink,
+      color: activeItem,
     ),
   ),
   const BottomBarItem(
     inActiveItem: FaIcon(
       FontAwesomeIcons.gear,
-      color: Colors.blueGrey,
+      color: inActiveItem,
     ),
     activeItem: FaIcon(
       FontAwesomeIcons.gear,
-      color: Colors.yellow,
+      color: activeItem,
     ),
   ),
 ];
