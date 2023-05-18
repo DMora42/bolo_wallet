88a81dcba3a2dc6b37f04f79658894f527d042a5
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-05-18 10:54:03 -0400

    Create common_token_list.dart

---
+++ lib/provider/binance/common_token_list.dart
@@ -0,0 +1,178 @@
+List binance_tokens = [
+  {
+    "name": "Binance Coin",
+    "address": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+  },
+  {
+    "name": "Ethereum",
+    "address": "0x2170Ed0880ac9A755fd29B2688956BD959F933F8"
+  },
+  {
+    "name": "Bitcoin",
+    "address": "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c"
+  },
+  {
+    "name": "Tether",
+    "address": "0x55d398326f99059fF775485246999027B3197955"
+  },
+  {
+    "name": "Cardano",
+    "address": "0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47"
+  },
+  {
+    "name": "XRP",
+    "address": "0x1D2F0da169ceB9fC7B3144628dB156f3F6c60dBE"
+  },
+  {
+    "name": "Solana",
+    "address": "0x7Dc8fA3C0E84b1b1fB3cEBdAa91cAfF7967a1Dd6"
+  },
+  {
+    "name": "Polkadot",
+    "address": "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402"
+  },
+  {
+    "name": "Dogecoin",
+    "address": "0xbA2aE424d960c26247Dd6c32edC70B295c744C43"
+  },
+  {
+    "name": "USD Coin",
+    "address": "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"
+  },
+  {
+    "name": "Cosmos",
+    "address": "0x0Eb3a705fc54725037CC9e008bDede697f62F335"
+  },
+  {
+    "name": "Avalanche",
+    "address": "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7"
+  },
+  {
+    "name": "Chainlink",
+    "address": "0xf8A0BF9cF54Bb92F17374d9e9A321E6a111a51bD"
+  },
+  {
+    "name": "Fantom",
+    "address": "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"
+  },
+  {
+    "name": "Internet Computer",
+    "address": "0xd4fbc14C5122cE2B3d8611D15E3Cb7014181BF34"
+  },
+  {
+    "name": "Polygon",
+    "address": "0xcc42724C6683B7E57334c4E856f4c9965ED682bD"
+  },
+  {
+    "name": "Algorand",
+    "address": "0x0000000000000000000000000000000000000000"
+  },
+  {
+    "name": "Elrond",
+    "address": "0xbF7c81FFF98BbE61B40Ed186e4AfD6DDd01337fe"
+  },
+  {
+    "name": "Stellar",
+    "address": "0x50D1c9771902476076eCFc8B2A83Ad6b9355a4c9"
+  },
+  {
+    "name": "THORChain",
+    "address": "0x3155BA85D5F96b2d030a4966AF206230e46849cb"
+  },
+  {
+    "name": "Shiba Inu",
+    "address": "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE"
+  },
+  {
+    "name": "BitTorrent",
+    "address": "0x8595F9dA7b868b1822194fAEd312235E43007b49"
+  },
+  {
+    "name": "TRON",
+    "address": "0x85EAC5Ac2F758618dFa09bDbe0cf174e7d574D5B"
+  },
+  {
+    "name": "Hedera Hashgraph",
+    "address": "0xEd6BF4859A2d14dA60E0F1fB7b8c8cd4468A2e64"
+  },
+  {
+    "name": "SushiSwap",
+    "address": "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"
+  },
+  {
+    "name": "Uniswap",
+    "address": "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
+  },
+  {
+    "name": "Compound",
+    "address": "0x580DCE4061a33083466F11728DDe028956ABcFD0"
+  },
+  {
+    "name": "Amp",
+    "address": "0xff20817765CB7f73d4bde2e66e067E58D11095C2"
+  },
+  {
+    "name": "Maker",
+    "address": "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2"
+  },
+  {
+    "name": "Curve DAO Token",
+    "address": "0xD533a949740bb3306d119CC777fa900bA034cd52"
+  },
+  {
+    "name": "Polygon Network",
+    "address": "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0"
+  },
+  {
+    "name": "The Graph",
+    "address": "0xc944E90C64B2c07662A292be6244BDf05Cda44a7"
+  },
+  {
+    "name": "Harmony",
+    "address": "0x82f0B8B456c1A451378467398982d4834b6829c1"
+  },
+  {
+    "name": "Holo",
+    "address": "0x6c6EE5e31d828De241282B9606C8e98Ea48526E2"
+  },
+  {
+    "name": "Nexo",
+    "address": "0xB62132e35a6c13ee1EE0f84dC5d40bad8d815206"
+  },
+  {
+    "name": "PancakeSwap",
+    "address": "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"
+  },
+  {
+    "name": "Filecoin",
+    "address": "0xF3b3Cad094B89392fcE5faFD40bC03b80F2Bc624"
+  },
+  {
+    "name": "Compound Dai",
+    "address": "0x5BC25f649fc4e26069dDF4cF4010F9f706c23831"
+  },
+  {
+    "name": "Huobi Token",
+    "address": "0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F"
+  },
+  {
+    "name": "Aave",
+    "address": "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9"
+  },
+  {
+    "name": "The Sandbox",
+    "address": "0x4d5a316D23eBE168d8f887b4447bf8DbFA4901CC"
+  },
+  {
+    "name": "Chiliz",
+    "address": "0x3506424F91fD33084466F402d5D97f05F8e3b4AF"
+  },
+  {
+    "name": "EOS",
+    "address": "0x56b6fB708fC5732DEC1Afc8D8556423A2EDcCbD6"
+  },
+  {
+    "name": "Celo",
+    "address": "0x471EcE3750Da237f93B8E339c536989b8978a438"
+  },
+];
